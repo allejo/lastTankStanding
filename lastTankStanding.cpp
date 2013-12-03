d2c07b6ae81db90726346f519f9239136ad08010
allejo <allejo@me.com>
2013-12-03 00:09:36 -0800

    And added more functionality...

--- lastTankStanding.cpp
+++ lastTankStanding.cpp
@@ -1,3 +1,5 @@
+#include <time.h>
+
 #include "bzfsAPI.h"
 #include "bzToolkitAPI.h"
 
@@ -5,6 +7,52 @@ void resetPlayerScore(int playerID)
 {
     bz_setPlayerWins(playerID, 0);
     bz_setPlayerLosses(playerID, 0);
+}
+
+int getLastTankStanding()
+{
+    int lastTankStanding = -1;
+
+    bz_APIIntList *playerList = bz_newIntList();
+    bz_getPlayerIndexList(playerList);
+
+    for (unsigned int i = 0; i < playerList->size(); i++)
+    {
+        if (bz_getPlayerByIndex(playerList->get(i))->spawned)
+        {
+            lastTankStanding = playerList->get(i);
+        }
+    }
+
+    bz_deleteIntList(playerList);
+    return lastTankStanding;
+}
+
+int getPlayerWithLowestScore()
+{
+    int playerWithLowestScore = -1;
+    int lowestHighestScore = 9999;
+
+    bz_APIIntList *playerList = bz_newIntList();
+    bz_getPlayerIndexList(playerList);
+
+    for (unsigned int i = 0; i < playerList->size(); i++)
+    {
+        int playerScore = bz_getPlayerWins(playerList->get(i)) - bz_getPlayerLosses(playerList->get(i));
+
+        if (playerScore < lowestHighestScore)
+        {
+            lowestHighestScore = playerScore;
+            playerWithLowestScore = playerList->get(i);
+        }
+        else if (playerScore == lowestHighestScore)
+        {
+            return -1;
+        }
+    }
+
+    bz_deleteIntList(playerList);
+    return playerWithLowestScore;
 }
 
 class lastTankStanding : public bz_Plugin, bz_CustomSlashCommandHandler
@@ -17,8 +65,25 @@ public:
 
     virtual bool SlashCommand(int playerID, bz_ApiString, bz_ApiString, bz_APIStringList*);
 
-    bool isCountdownInProgress, isGameInProgress;
-    int countdownLength, kickTime;
+    double
+        startOfMatch,
+        bzdb_gravity,
+        bzdb_jumpVelocity,
+        bzdb_reloadTime,
+        bzdb_tankSpeed,
+        bzdb_tankAngVel;
+
+    bool
+        isCountdownInProgress,
+        isGameInProgress;
+
+    int
+        countdownLength,
+        kickTime,
+        countdownProgress;
+
+    time_t
+        lastCountdownCheck;
 };
 
 BZ_PLUGIN(lastTankStanding)
@@ -30,14 +95,15 @@ void lastTankStanding::Init(const char* commandLine)
     isCountdownInProgress = false;
     isGameInProgress = false;
     countdownLength = 15;
+    startOfMatch = 0;
     kickTime = 60;
 
     Register(bz_eBZDBChange);
     Register(bz_ePlayerJoinEvent);
     Register(bz_eTickEvent);
 
-    bz_setBZDBInt("_ltsKickTime", kickTime, 0, false);
-    bz_setBZDBInt("_ltsCountDown", countdownLength, 0, false);
+    bztk_registerCustomIntBZDB("_ltsKickTime", kickTime);
+    bztk_registerCustomIntBZDB("_ltsCountdown", countdownLength);
 
     bz_registerCustomSlashCommand("start", this);
     bz_registerCustomSlashCommand("end", this);
@@ -70,7 +136,7 @@ void lastTankStanding::Event(bz_EventData *eventData)
                     kickTime = 60;
                 }
             }
-            else if (bzdbChange->key == "_ltsCountDown")
+            else if (bzdbChange->key == "_ltsCountdown")
             {
                 if (atoi(bzdbChange->value.c_str()) > 0)
                 {
@@ -78,7 +144,7 @@ void lastTankStanding::Event(bz_EventData *eventData)
                 }
                 else
                 {
-                    countdownLength = 60;
+                    countdownLength = 15;
                 }
             }
         }
@@ -101,7 +167,57 @@ void lastTankStanding::Event(bz_EventData *eventData)
 
         case bz_eTickEvent:
         {
+            if (isCountdownInProgress)
+            {
+                time_t currentTime;
+                time(&currentTime);
 
+                if (difftime(currentTime, lastCountdownCheck) > 1)
+                {
+                    if (countdownProgress < 1)
+                    {
+                        isCountdownInProgress = false;
+                        isGameInProgress = true;
+
+                        bz_setBZDBDouble("_gravity", bzdb_gravity);
+                        bz_setBZDBDouble("_jumpVelocity", bzdb_jumpVelocity);
+                        bz_setBZDBDouble("_reloadTime", bzdb_reloadTime);
+                        bz_setBZDBDouble("_tankAngVel", bzdb_tankAngVel);
+                        bz_setBZDBDouble("_tankSpeed", bzdb_tankSpeed);
+
+                        bz_sendTextMessage(BZ_SERVER, BZ_ALLUSERS, "The game has started. Good luck!");
+                        bz_sendTextMessagef(BZ_SERVER, BZ_ALLUSERS, "The player at the bottom of the scoreboard will be removed every %d seconds.", kickTime);
+                    }
+                    else
+                    {
+                        bz_sendTextMessagef(BZ_SERVER, BZ_ALLUSERS, "%i", countdownProgress);
+                        time(&lastCountdownCheck);
+                        countdownProgress--;
+                    }
+                }
+            }
+
+            if (isGameInProgress)
+            {
+                if (bz_getPlayerCount() > 1)
+                {
+
+                }
+                else if (bz_getPlayerCount() == 1)
+                {
+                    bz_BasePlayerRecord *lastTankStanding = bz_getPlayerByIndex(getLastTankStanding());
+                    bz_sendTextMessagef(BZ_SERVER, BZ_ALLUSERS, "Last Tank Standing is over! The winner is \"%s\" with a score of %d", lastTankStanding->callsign.c_str(), (lastTankStanding->wins - lastTankStanding->losses));
+
+                    isCountdownInProgress = false;
+                    isGameInProgress = false;
+                }
+                else
+                {
+                    // Meh?
+                    isCountdownInProgress = false;
+                    isGameInProgress = false;
+                }
+            }
         }
 
         default: break;
@@ -112,26 +228,58 @@ bool lastTankStanding::SlashCommand(int playerID, bz_ApiString command, bz_ApiSt
 {
     if (command == "start" && bz_hasPerm(playerID, "vote"))
     {
-        if (!isGameInProgress && !isCountdownInProgress)
+        if (!isGameInProgress && !isCountdownInProgress && bztk_getPlayerCount() > 2)
         {
             isCountdownInProgress = true;
+            time(&lastCountdownCheck);
+
+            countdownProgress = countdownLength;
 
             bz_sendTextMessagef(BZ_SERVER, BZ_ALLUSERS, "%s started a new game of Last Tank Standing. New players are now unable to join.", bz_getPlayerByIndex(playerID)->callsign.c_str());
 
             bztk_foreachPlayer(resetPlayerScore);
             bz_sendTextMessagef(BZ_SERVER, BZ_ALLUSERS, "All scores have been reset.");
 
+            bzdb_gravity = bz_getBZDBDouble("_gravity");
+            bzdb_jumpVelocity = bz_getBZDBDouble("_jumpVelocity");
+            bzdb_reloadTime = bz_getBZDBDouble("_reloadTime");
+            bzdb_tankAngVel = bz_getBZDBDouble("_tankAngVel");
+            bzdb_tankSpeed = bz_getBZDBDouble("_tankSpeed");
+
             bz_setBZDBDouble("_gravity", -1000.000000);
             bz_setBZDBDouble("_jumpVelocity", 0.000000);
             bz_setBZDBDouble("_reloadTime", 0.1);
-            bz_setBZDBDouble("_tankSpeed", 0.000001);
             bz_setBZDBDouble("_tankAngVel", 0.000001);
-
-            return true;
+            bz_setBZDBDouble("_tankSpeed", 0.000001);
         }
+        else if (isCountdownInProgress)
+        {
+            bz_sendTextMessage(BZ_SERVER, playerID, "There is already a countdown in progress.");
+        }
+        else if (isGameInProgress)
+        {
+            bz_sendTextMessage(BZ_SERVER, playerID, "There is already a game of Last Tank Standing in progress.");
+        }
+        else if (bztk_getPlayerCount() <= 2)
+        {
+            bz_sendTextMessage(BZ_SERVER, playerID, "More than 2 players are required to play a game of Last Tank Standing.");
+        }
+
+        return true;
     }
     else if (command == "end" && bz_hasPerm(playerID, "gameover"))
     {
+        bz_sendTextMessagef(BZ_SERVER, BZ_ALLUSERS, "%s has ended the current game of Last Tank Standing.", bz_getPlayerByIndex(playerID)->callsign.c_str());
 
+        isCountdownInProgress = false;
+        isGameInProgress = false;
+
+        return true;
+    }
+
+    if (command == "start" || command == "end")
+    {
+        bz_sendTextMessagef(BZ_SERVER, playerID, "You do not have permission to use the /%s command.", command.c_str());
+        return true;
     }
 }
